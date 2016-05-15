d6d1067d6f5b1d33c9e6667bf9eb3015c36c5116
smcl <sean.mclemon@gmail.com>
2016-05-15 21:49:18 +0200

    gps and correct pin unlock

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import time
  
 ctrlZ = chr(26)
 
+smsTimeout = 2
+gpsTimeout = 5
+
 class Modem(object):
 	def __init__(self, dev, bps=460800, pin=None):
 		self.device = serial.Serial(dev, bps, timeout=1)
@@ -29,15 +32,35 @@ class Modem(object):
 				pinNeeded = False
 
 		if pinNeeded:
-			self.AT("AT+CPIN=%d" % (pin))
+			self.AT("AT+CPIN=\"%s\"" % (pin))
 
 	def sendSMS(self, number, message):
-		self.AT("AT+CMGS=\"%s\"\r" % (number))
+		self.AT("AT+CMGS=\"%s\"" % (number))
 		self.AT(message)
 		self.raw(ctrlZ)
-		time.sleep(2)
+		time.sleep(smsTimeout)
 		return self.device.readlines()
 
+	def getGPS(self):
+		self.AT("AT+XLCSLSR=1,1,,,,,,,,,,")
+		
+		gpsFix = False
+		attempts = 0
+
+		while attempts < 5:
+			time.sleep(gpsTimeout)
+			gpsLines = self.device.readlines()
+			for line in gpsLines:
+				if "XLCSLSR" in line and "XLCSLSR: request" not in line:
+					return gpsLines
+			attempts += 1
+
+		return []
 
 em7345 = Modem("/dev/ttyACM0", pin="1234")
 em7345.sendSMS("+420775356278", "test message from py!")
+gps = em7345.getGPS()
+
+if gps:
+        print(gps[1].split(",")[1])
+        print(gps[1].split(",")[2])
