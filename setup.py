1c9661ba67f4901635edc135b9eac5e6109d1bb4
smcl <sean.mclemon@gmail.com>
2016-10-21 20:31:57 +0200

    basic test, CI and coveralls setup - not working currently cos i have no tests and github is down

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+current_version = '0.1'
+
+setup(
+    name = 'em73xx',
+    packages = [ 'em73xx', 'em73xx.test' ],
+    version = current_version,
+    description = 'python wrapper for the em73xx series of 4G modems present in recent Lenovo Thinkpads',
+    author = 'Sean McLemon',
+    author_email = 'sean.mclemon@gmail.com',
+    url = 'https://github.com/smcl/py-em73xx',
+    download_url = 'https://github.com/smcl/py-em73xx/tarball/%s' % (current_version),
+    keywords = ['thinkpad', 'em7345', 'em73xx'],
+    classifiers = [],
+    test_suite='em73xx.test.all',
+    install_requires=[
+        'unittest2'
+    ],
+    setup_requires=[
+        'unittest2'
+    ],
+    
+)
