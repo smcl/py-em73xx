858bfe6ac75c4c867edceacc07bb39e03faf5999
smcl <sean.mclemon@gmail.com>
2016-10-22 00:04:17 +0200

    py3 has no izip, just use zip

--- em73xx/utils.py
+++ em73xx/utils.py
@@ -1,10 +1,7 @@
-from itertools import izip
-
-
 def pairwise(iterable):
     "s -> (s0, s1), (s2, s3), (s4, s5), ..."
     a = iter(iterable)
-    return izip(a, a)
+    return zip(a, a)
 
 
 def quote(s):
